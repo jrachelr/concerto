6a36d8d963bda966617e631a9d5aac8e93eb36d0
esteban <esteban.chirinos@gmail.com>
2022-11-16 19:56:14 -0500

    added routers and queries. Still need to fix them

--- concerts-api/main.py
+++ concerts-api/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from routers import concerts
 
 app = FastAPI()
+
+app.include_router(concerts.router)
--- concerts-api/routers/concerts.py
+++ concerts-api/routers/concerts.py
@@ -1,39 +1,28 @@
-# from fastapi import APIRouter, Depends
-# from pydantic import BaseModel
-# from datetime import date
-# from queries import ConcertQueries
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from datetime import date
+from queries.concert_queries import ConcertIn, ConcertOut, ConcertsList, ConcertQueries
 
-# router = APIRouter()
+router = APIRouter()
 
-# class ConcertIn(BaseModel):
-
-#     name: str
-#     artist: str
-#     dates: date
-#     min_price: int
-#     max_price: int
-
-# class ConcertOut(BaseModel):
-#     id: int
-#     name: str
-#     artist: str
-#     dates: date
-#     min_price: int
-#     max_price: int
+#when we create a concert
 
 
-# class ConcertsList(BaseModel):
-#     concerts: list(ConcertOut)
+@router.get('/concerts', response_model=ConcertsList)
+def get_all_concerts(queries:ConcertQueries = Depends()):
+    return {"concerts": queries.get_all_favorites()}
 
-# @router.get('/concerts/{id}', response_model=ConcertOut):
-# def get_concert_by_id(
-#     concert_id: int,
-#     queries: ConcertQueries = Depends()
-# ):
-#     pass
+@router.get('/concerts/{concert_id}', response_model=ConcertOut)
+def get_concert_by_id(concert_id:int, queries:ConcertQueries = Depends()):
+    return queries.get_one_concert(concert_id)
+
+@router.post('/concerts', response_model=ConcertIn)
+def post_concert(concert:ConcertIn, queries:ConcertQueries = Depends()):
+    return queries.add_favorite_concert(concert)
 
 
-# @router.get('/concerts', response_model=ConcertsOut)
+# @router.get('/concerts', response_model=ConcertsList)
 # def get_concerts(queries: ConcertQueries = Depends()):
-#     concerts = queries.get_all_concerts()
-#     return {'concerts': concerts}
+#     # concerts = queries.get_all_concerts()
+#     # return {'concerts': concerts}
+#     pass
