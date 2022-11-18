233a410b94ef12e8a8ce8f8e5e546bfddec6ac61
Nicolette <nmabeza@gmail.com>
2022-11-17 18:37:55 -0800

    refactoring get request

--- concerts-api/routers/concerts.py
+++ concerts-api/routers/concerts.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from datetime import date
 from queries.concert_queries import ConcertIn, ConcertOut, ConcertsList, ConcertQueries
+import requests
+import json
 
 router = APIRouter()
 
 #when we create a concert
-@router.get('/concerts', response_model=ConcertsList)
+@router.get('/concerts/favorites', response_model=ConcertsList)
 def get_all_concerts(queries:ConcertQueries = Depends()):
     return {"concerts": queries.get_all_favorites()}
 
@@ -14,6 +16,6 @@ def get_all_concerts(queries:ConcertQueries = Depends()):
 def get_concert_by_id(concert_id:int, queries:ConcertQueries = Depends()):
     return queries.get_one_concert(concert_id)
 
-@router.post('/concerts', response_model=ConcertOut)
+@router.post('/concerts/favorites', response_model=ConcertOut)
 def post_concert(concert:ConcertIn, queries:ConcertQueries = Depends()):
     return queries.add_favorite_concert(concert)
