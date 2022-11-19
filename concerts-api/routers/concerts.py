1b0acaf3503cf1ed5c0498e473af02f36dabf4dd
Nicolette <nmabeza@gmail.com>
2022-11-18 17:40:04 -0800

    refactored concert endpoints to include user id

--- concerts-api/routers/concerts.py
+++ concerts-api/routers/concerts.py
@@ -7,32 +7,35 @@ import json
 
 router = APIRouter()
 
-#when we create a concert
-@router.get('/concerts/favorites', response_model=ConcertsList)
+#add favorite concert
+@router.post("/concerts/favorites/{user_id}", response_model=ConcertOut)
+def post_favorite_concert(concert:ConcertIn, user_id: int, queries:ConcertQueries = Depends()):
+    return queries.create(concert, user_id)
+
+#get favorite concerts for all users
+@router.get('/concerts/favorites/', response_model=ConcertsList)
 def get_favorite_concerts(queries:ConcertQueries = Depends()):
     return {"concerts": queries.get_all()}
 
+#get favorite concerts for a specific user
+@router.get('/concerts/favorites/{user_id}', response_model=ConcertsList)
+def get_favorite_concerts(user_id: int, queries:ConcertQueries = Depends()):
+    return {"concerts": queries.get_all(user_id)}
 
-@router.get("/concerts/favorites/{concert_id}", response_model=ConcertOut)
-def get_favorite_concert_by_id(concert_id: int, queries: ConcertQueries = Depends()):
-    return queries.get_one(concert_id)
+#get one favorite concert for a specific user
+@router.get("/concerts/favorites/{user_id}/{concert_id}", response_model=ConcertOut)
+def get_favorite_concert_by_id(concert_id: int, user_id: int, queries: ConcertQueries = Depends()):
+    return queries.get_one(concert_id, user_id)
 
-@router.post("/concerts/favorites", response_model=ConcertOut)
-def post_favorite_concert(concert:ConcertIn, queries:ConcertQueries = Depends()):
-    return queries.create(concert)
-
-
-@router.put("/concerts/favorites/{concert_id}", response_model=ConcertOut)
+@router.put("/concerts/favorites/{user_id}/{concert_id}", response_model=ConcertOut)
 def update_favorite_concert(
     concert_id: int, concert: ConcertIn, queries: ConcertQueries = Depends()
 ):
     return queries.update(concert_id, concert)
 
-
-@router.delete("/concerts/favorites/{concert_id}", response_model=bool)
+@router.delete("/concerts/favorites/{user_id}/{concert_id}", response_model=bool)
 def delete_concert(concert_id: int, queries: ConcertQueries = Depends()):
     return queries.delete(concert_id)
-
 
 
 url = "https://app.ticketmaster.com/discovery/v2/events"
@@ -44,13 +47,14 @@ params = {"classificationName":"music",
 response = requests.get(url, params=params)
 content = json.loads(response.content)
 
+#get concerts from ticketmaster
 async def get_ticketmaster_concerts():
   response = requests.get(url, params=params)
   content = json.loads(response.content)
   return content
 
 @router.get("/concerts")
-def get_all_concerts(data: get_ticket_master_concerts = Depends()):
+def get_all_concerts(data: get_ticketmaster_concerts = Depends()):
     events = content['_embedded']['events']
     concerts=[]
     for event in events:
