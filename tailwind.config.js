cb59d4dd4a5dbebeda66bc2280de82d349d82ba6
rachel <jrachelr1@gmail.com>
2022-11-22 16:53:23 -0800

    pushing tw to get user code

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            './public/index.html',],
+  theme: {
+    
+    extend: {fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
