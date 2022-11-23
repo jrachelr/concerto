76a54acfb87239d18041d48d980d6a4a1442968b
Nicolette <nmabeza@gmail.com>
2022-11-22 17:52:57 -0800

    fixed tickmaster get request

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,38 +1,13 @@
-import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
-import ErrorNotification from './ErrorNotification';
-import './App.css';
+import "./App.css";
+import SearchComponent from "./SearchComponent";
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
-
-  return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
-  );
+	return (
+		<>
+			<h1>Concerto</h1>
+			<SearchComponent />
+		</>
+	);
 }
 
 export default App;
---
+++ ghi/src/SearchComponent.jsx
@@ -0,0 +1,69 @@
+import usePlacesAutocomplete, {
+	getGeocode,
+	getLatLng,
+} from "use-places-autocomplete";
+
+const SearchComponent = () => {
+	const {
+		ready,
+		value,
+		suggestions: { status, data },
+		setValue,
+		clearSuggestions,
+	} = usePlacesAutocomplete({
+		requestOptions: {
+			/* Define search scope here */
+		},
+		debounce: 300,
+	});
+
+	const handleInput = (e) => {
+		// Update the keyword of the input element
+		setValue(e.target.value);
+	};
+
+	const handleSelect =
+		({ description }) =>
+		() => {
+			// When user selects a place, we can replace the keyword without request data from API
+			// by setting the second parameter to "false"
+			setValue(description, false);
+			clearSuggestions();
+
+			// Get latitude and longitude via utility functions
+			getGeocode({ address: description }).then((results) => {
+				const { lat, lng } = getLatLng(results[0]);
+				console.log("Coordinates: ", { lat, lng });
+			});
+		};
+
+	const renderSuggestions = () =>
+		data.map((suggestion) => {
+			const {
+				place_id,
+				structured_formatting: { main_text, secondary_text },
+			} = suggestion;
+
+			return (
+				<li key={place_id} onClick={handleSelect(suggestion)}>
+					<strong>{main_text}</strong> <small>{secondary_text}</small>
+				</li>
+			);
+		});
+
+	return (
+		<div>
+			<input
+				id="autocomplete"
+				value={value}
+				onChange={handleInput}
+				disabled={!ready}
+				placeholder="Where are you going?"
+			/>
+			{/* We can use the "status" to decide whether we should display the dropdown or not */}
+			{status === "OK" && <ul>{renderSuggestions()}</ul>}
+		</div>
+	);
+};
+
+export default SearchComponent;
