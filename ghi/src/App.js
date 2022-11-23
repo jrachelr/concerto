f72c071cd79c13830449e064a82225ac2f8d5841
rachel <jrachelr1@gmail.com>
2022-11-22 16:51:09 -0800

    added tw stuff SOS

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,37 +1,17 @@
-import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
-import ErrorNotification from './ErrorNotification';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Login from './Login';
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
+  
 
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
+
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
