a5bc946dc91e07a541e5684891a14f5f4b8427e9
esteban <esteban.chirinos@gmail.com>
2022-11-30 15:17:24 -0500

    working auth for user

--- users-api/main.py
+++ users-api/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 from routers import users
 from authenticator import authenticator
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get(
+            "CORS_HOST",
+            "REACT_APP_SAMPLE_SERVICE_API_HOST",
+        ), "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.include_router(users.router)
