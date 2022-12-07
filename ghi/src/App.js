be9dd8142f95d9c3eb4f7caadb1a42cb53973659
Nicolette <nmabeza@gmail.com>
2022-12-07 08:43:48 -0800

    Fixed 401 auth error on landing page and infinite fetch on user favorites

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -14,69 +14,57 @@ import Favorites from "./Users/ConcertFavorites";
 import AccountInfo from "./Users/Account";
 
 function GetToken() {
-  // Get token from JWT cookie (if already logged in)
-  useToken();
-  return null;
+	// Get token from JWT cookie (if already logged in)
+	useToken();
+	return null;
 }
 
 function App() {
-  const [token, login, logout, signup] = useToken();
-  const [concerts, setConcerts] = useState([]);
+	const [concerts, setConcerts] = useState([]);
 
-  async function getConcerts(city, state) {
-    const concertsUrl = `http://localhost:8000/concerts/${city},${state}`;
-    const fetchConfig = {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+	async function getConcerts(city, state) {
+		const concertsUrl = `http://localhost:8000/concerts/${city},${state}`;
+		const fetchConfig = {
+			method: "get",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-    const response = await fetch(concertsUrl, fetchConfig);
-    if (response.ok) {
-      const data = await response.json();
-      setConcerts(data.concerts);
-    } else {
-      console.log("SOS");
+		const response = await fetch(concertsUrl, fetchConfig);
+		if (response.ok) {
+			const data = await response.json();
+			setConcerts(data.concerts);
+		} else {
+			console.log("SOS");
+		}
+	}
 
-      // const setLocation = (data) => {
-      // 	setConcerts(data);
-      // };
-    }
-  }
-
-  return (
-    <>
-      <AuthProvider>
-        <GetToken />
-        <Routes>
-          <Route path="/" element={<Landing />} token={token} />
-          <Route path="header/" element={<Header />} token={token} />
-          <Route path="sidebar/" element={<SideBar />} token={token} />
-          <Route path="myconcerts/" element={<Favorites />} token={token} />
-          <Route path="myaccount/" element={<AccountInfo />} token={token} />
-          <Route
-            path="login/"
-            element={<LoginForm token={token} login={login} />}
-          />
-          <Route
-            path="signup/"
-            element={<SignupForm token={token} signup={signup} />}
-          />
-          <Route path="logout/" element={<Logout />} logout={logout} />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route
-            path="search"
-            element={<SearchComponent getConcerts={getConcerts} />}
-          />
-          <Route
-            path="concerts"
-            element={<ConcertList concerts={concerts} />}
-          />
-        </Routes>
-      </AuthProvider>
-    </>
-  );
+	return (
+		<>
+			<AuthProvider>
+				<GetToken />
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="header/" element={<Header />} />
+					<Route path="sidebar/" element={<SideBar />} />
+					<Route path="myconcerts/" element={<Favorites />} />
+					<Route path="login/" element={<LoginForm />} />
+					<Route path="signup/" element={<SignupForm />} />
+					<Route path="logout/" element={<Logout />} />
+					<Route path="*" element={<Navigate to="/" />} />
+					<Route
+						path="search"
+						element={<SearchComponent getConcerts={getConcerts} />}
+					/>
+					<Route
+						path="concerts"
+						element={<ConcertList concerts={concerts} />}
+					/>
+				</Routes>
+			</AuthProvider>
+		</>
+	);
 }
 
 export default App;
--- ghi/src/auth.js
+++ ghi/src/auth.js
@@ -3,170 +3,170 @@ import { useNavigate } from "react-router-dom";
 let internalToken = null;
 
 export function getToken() {
-  return internalToken;
+	return internalToken;
 }
 
 export async function getTokenInternal() {
-  const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token/`;
-  try {
-    const response = await fetch(url, {
-      credentials: "include",
-    });
-    if (response.ok) {
-      const data = await response.json();
-      internalToken = data.access_token;
-      return internalToken;
-    }
-  } catch (e) {}
-  return false;
+	const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token/`;
+	try {
+		const response = await fetch(url, {
+			credentials: "include",
+		});
+		if (response.ok) {
+			const data = await response.json();
+			internalToken = data.access_token;
+			return internalToken;
+		}
+	} catch (e) {}
+	return false;
 }
 
 function handleErrorMessage(error) {
-  if ("error" in error) {
-    error = error.error;
-    try {
-      error = JSON.parse(error);
-      if ("__all__" in error) {
-        error = error.__all__;
-      }
-    } catch {}
-  }
-  if (Array.isArray(error)) {
-    error = error.join("<br>");
-  } else if (typeof error === "object") {
-    error = Object.entries(error).reduce(
-      (acc, x) => `${acc}<br>${x[0]}: ${x[1]}`,
-      ""
-    );
-  }
-  return error;
+	if ("error" in error) {
+		error = error.error;
+		try {
+			error = JSON.parse(error);
+			if ("__all__" in error) {
+				error = error.__all__;
+			}
+		} catch {}
+	}
+	if (Array.isArray(error)) {
+		error = error.join("<br>");
+	} else if (typeof error === "object") {
+		error = Object.entries(error).reduce(
+			(acc, x) => `${acc}<br>${x[0]}: ${x[1]}`,
+			""
+		);
+	}
+	return error;
 }
 
 export const AuthContext = createContext({
-  token: null,
-  setToken: () => null,
-  user: null,
-  setUser: () => null,
+	token: null,
+	setToken: () => null,
+	user: null,
+	setUser: () => null,
 });
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
-  const [user, setUser] = useState(null);
+	const [token, setToken] = useState(null);
+	const [user, setUser] = useState(null);
 
-  return (
-    <AuthContext.Provider value={{ token, setToken, user, setUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider value={{ token, setToken, user, setUser }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export function useToken() {
-  const { token, setToken, user, setUser } = useAuthContext();
-  const navigate = useNavigate();
+	const { token, setToken, user, setUser } = useAuthContext();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    async function fetchToken() {
-      const token = await getTokenInternal();
-      setToken(token);
-      const response2 = await fetch(
-        `${process.env.REACT_APP_ACCOUNTS_HOST}/users/current`,
-        {
-          method: "get",
-          credentials: "include",
-        }
-      );
-      const response3 = await response2.json();
-      setUser(response3);
-    }
-    if (!token) {
-      fetchToken();
-    }
-  }, [setToken, token, setUser]);
+	useEffect(() => {
+		async function fetchToken() {
+			const token = await getTokenInternal();
+			setToken(token);
+			// const response2 = await fetch(
+			// 	`${process.env.REACT_APP_ACCOUNTS_HOST}/users/current`,
+			// 	{
+			// 		method: "get",
+			// 		credentials: "include",
+			// 	}
+			// );
+			// const response3 = await response2.json();
+			// setUser(response3);
+		}
+		if (!token) {
+			fetchToken();
+		}
+	}, [setToken, token, setUser]);
 
-  async function logout() {
-    if (token) {
-      const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token/`;
-      await fetch(url, { method: "delete", credentials: "include" });
-      internalToken = null;
-      setToken(null);
-      setUser(null);
-      navigate("/");
-    }
-  }
+	async function logout() {
+		if (token) {
+			const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token/`;
+			await fetch(url, { method: "delete", credentials: "include" });
+			internalToken = null;
+			setToken(null);
+			setUser(null);
+			navigate("/");
+		}
+	}
 
-  async function login(username, password) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-    const form = new FormData();
+	async function login(username, password) {
+		const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
+		const form = new FormData();
 
-    form.append("username", username);
-    form.append("password", password);
-    const response = await fetch(url, {
-      method: "post",
-      credentials: "include",
-      body: form,
-    });
-    const response2 = await fetch(
-      `${process.env.REACT_APP_ACCOUNTS_HOST}/users/current`,
-      {
-        method: "get",
-        credentials: "include",
-      }
-    );
-    setUser(await response2.json());
+		form.append("username", username);
+		form.append("password", password);
+		const response = await fetch(url, {
+			method: "post",
+			credentials: "include",
+			body: form,
+		});
+		const response2 = await fetch(
+			`${process.env.REACT_APP_ACCOUNTS_HOST}/users/current`,
+			{
+				method: "get",
+				credentials: "include",
+			}
+		);
+		setUser(await response2.json());
 
-    if (response.ok) {
-      const token = await getTokenInternal();
-      setToken(token);
-      return;
-    }
-    let error = await response.json();
-    return handleErrorMessage(error);
-  }
+		if (response.ok) {
+			const token = await getTokenInternal();
+			setToken(token);
+			return;
+		}
+		let error = await response.json();
+		return handleErrorMessage(error);
+	}
 
-  async function signup(username, password, email, firstName, lastName) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/users`;
-    const response = await fetch(url, {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        password,
-        email,
-        first_name: firstName,
-        last_name: lastName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      await login(email, password);
-    }
-    console.log("respose:", response);
-    return false;
-  }
+	async function signup(username, password, email, firstName, lastName) {
+		const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/users`;
+		const response = await fetch(url, {
+			method: "post",
+			body: JSON.stringify({
+				username,
+				password,
+				email,
+				first_name: firstName,
+				last_name: lastName,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		if (response.ok) {
+			await login(email, password);
+		}
+		console.log("respose:", response);
+		return false;
+	}
 
-  async function update(username, password, email, firstName, lastName) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/accounts/`;
-    const response = await fetch(url, {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        password,
-        email,
-        first_name: firstName,
-        last_name: lastName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      await login(username, password);
-    }
-    return false;
-  }
+	async function update(username, password, email, firstName, lastName) {
+		const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/accounts/`;
+		const response = await fetch(url, {
+			method: "post",
+			body: JSON.stringify({
+				username,
+				password,
+				email,
+				first_name: firstName,
+				last_name: lastName,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		if (response.ok) {
+			await login(username, password);
+		}
+		return false;
+	}
 
-  return [token, login, logout, signup, update, user];
+	return [token, login, logout, signup, update, user];
 }
