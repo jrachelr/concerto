68bf7922906cb662e3ded68fecabbf697620316f
esteban <esteban.chirinos@gmail.com>
2022-12-08 15:40:27 -0500

    fixed users database url

--- users-api/queries/user_queries.py
+++ users-api/queries/user_queries.py
@@ -41,7 +41,7 @@ class UsersList(BaseModel):
 
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
-conn = connect(conninfo = "postgres://gmdhwhkk:0aMxS-RRJfaZnoGa4cgenGnsw8rhsSnw@arjuna.db.elephantsql.com/gmdhwhkk", **keepalive_kwargs)
+conn = connect(conninfo=os.environ["USERS_DATABASE_URL"], **keepalive_kwargs)
 
 
 class UserQueries:
