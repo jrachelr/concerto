a3807552e1e2a7d9fe0c64b1e48482a96a8bf591
Nicolette <nmabeza@gmail.com>
2022-12-09 12:29:43 -0800

    pipeline fix

--- ghi/src/Users/Signup.js
+++ ghi/src/Users/Signup.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useToken } from "../auth.js";
-import { useNavigate } from "react-router-dom";
 import { GiHarp } from "react-icons/gi";
 
 const SignupForm = () => {
